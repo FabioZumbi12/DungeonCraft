39fdad14a2334717cc0e246929f906b144f24134
xXKeyleXx <keyle@t-online.de>
2014-04-18 14:24:33 +0200

    add class for health bar component (mostly empty)

---
+++ src/main/java/de/keyle/dungeoncraft/entity/template/components/HealthBarComponent.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of DungeonCraft
+ *
+ * Copyright (C) 2013-2014 Keyle & xXLupoXx
+ * DungeonCraft is licensed under the GNU Lesser General Public License.
+ *
+ * DungeonCraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DungeonCraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.keyle.dungeoncraft.entity.template.components;
+
+import de.keyle.dungeoncraft.api.entity.components.EntityTemplateComponent;
+import de.keyle.dungeoncraft.api.entity.components.Parameter;
+import de.keyle.dungeoncraft.entity.types.EntityDungeonCraft;
+
+public class HealthBarComponent extends EntityTemplateComponent {
+    boolean visible = true;
+
+    public HealthBarComponent(@Parameter(type = Parameter.Type.Boolean, name = "visible") boolean visible) {
+        this.visible = visible;
+    }
+
+    boolean isVisible() {
+        return visible;
+    }
+
+    @Override
+    public void applyComponent(EntityDungeonCraft entity) {
+    }
+}
